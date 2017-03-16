e36c83dfdd0f6f5708b1ed39a4a352b7cd934931
orlanth23 <orlanth23@hotmail.com>
2017-03-16 20:53:11 +1100

    Modification sur le sync Adapter

--- app/src/main/java/com/orlanth23/annoncesnc/sync/AnnoncesSyncAdapter.java
+++ app/src/main/java/com/orlanth23/annoncesnc/sync/AnnoncesSyncAdapter.java
@@ -1,6 +1,8 @@
 package com.orlanth23.annoncesnc.sync;
 
 import android.accounts.Account;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.AbstractThreadedSyncAdapter;
 import android.content.ContentProviderClient;
 import android.content.ContentResolver;
@@ -9,10 +11,12 @@ import android.content.Context;
 import android.content.SyncResult;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
+import com.orlanth23.annoncesnc.R;
 import com.orlanth23.annoncesnc.dto.StatutAnnonce;
 import com.orlanth23.annoncesnc.dto.StatutPhoto;
 import com.orlanth23.annoncesnc.list.ListeCategories;
@@ -30,14 +34,16 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
 import static com.orlanth23.annoncesnc.provider.AnnoncesProvider.sSelectionAnnoncesByStatut;
 import static com.orlanth23.annoncesnc.provider.AnnoncesProvider.sSelectionPhotosByStatut;
-import static com.orlanth23.annoncesnc.provider.ProviderContract.*;
+import static com.orlanth23.annoncesnc.provider.ProviderContract.AnnonceEntry;
+import static com.orlanth23.annoncesnc.provider.ProviderContract.PhotoEntry;
 
 public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
 
     // Création des classes privées pour récupérer des infos à partir des curseurs
-    private class AnnonceForWs{
+    private class AnnonceForWs {
         Integer idAnnonce;
         Integer idCategory;
         Integer idUtilisateur;
@@ -47,14 +53,14 @@ public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
         Integer idLocal;
     }
 
-    private class PhotoForWs{
+    private class PhotoForWs {
         Integer idAnnonce;
         Integer idLocal;
         Integer idPhoto;
         String nomPhoto;
     }
 
-    private class MessageForWs{
+    private class MessageForWs {
         Integer idMessage;
         Integer idLocal;
         Integer idSender;
@@ -63,55 +69,15 @@ public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
     }
 
     private static final String TAG = AnnoncesSyncAdapter.class.getName();
-
     private static final Gson gson = new Gson();
     private static final RetrofitService retrofitService = new Retrofit.Builder().baseUrl(Proprietes.getServerEndpoint()).addConverterFactory(GsonConverterFactory.create()).build().create(RetrofitService.class);
     private ContentResolver mContentResolver;
     private Context mContext;
-
-    private Callback<ReturnWS> callbackPostAnnonce = new Callback<ReturnWS>() {
-        @Override
-        public void onResponse(Call<ReturnWS> call, Response<ReturnWS> response) {
-            if (response.isSuccessful()) {
-                ReturnWS rs = response.body();
-                if (rs.statusValid()) {
-                    // Si la mise à jour à bien eu lieu sur le serveur, on va mettre à jour l'annonce dans notre ContentProvider
-                    Integer idLocal = rs.getIdLocal();
-                    ContentValues contentValues = new ContentValues();
-                    contentValues.put(AnnonceContract.COL_STATUT_ANNONCE, StatutAnnonce.Valid.valeur());
-                    String where = AnnonceContract._ID + " = ?";
-                    String[] whereArgs = new String[]{String.valueOf(idLocal)};
-                    mContentResolver.update(AnnonceEntry.CONTENT_URI, contentValues, where, whereArgs);
-                } else {
-                    Toast.makeText(getContext(), rs.getMsg(), Toast.LENGTH_LONG).show();
-                }
-            }
-        }
-
-        @Override
-        public void onFailure(Call<ReturnWS> call, Throwable t) {
-            t.printStackTrace();
-        }
-    };
-    private Callback<ReturnWS> callbackGetInfoServer = new Callback<ReturnWS>() {
-        @Override
-        public void onResponse(Call<ReturnWS> call, Response<ReturnWS> response) {
-            if (response.isSuccessful()) {
-                ReturnWS rs = response.body();
-                if (rs.statusValid()) {
-                    InfoServer infoServer = gson.fromJson(rs.getMsg(), InfoServer.class);
-                    ListeStats.setNbUsers(infoServer.getNbUtilisateur());
-                    ListeStats.setNbAnnonces(infoServer.getNbAnnonce());
-                    ListeCategories.setNbAnnonceFromHashMap(mContext,infoServer.getNbAnnonceByCategorie());
-                }
-            }
-        }
-
-        @Override
-        public void onFailure(Call<ReturnWS> call, Throwable t) {
-            Log.d(TAG, "callbackGetInfoServer failed");
-        }
-    };
+    private int nbAnnoncesDeleted;
+    private int nbAnnoncesSend;
+    private int nbPhotosSend;
+    private int nbMessageSend;
+    private String textToSend;
 
 
     public AnnoncesSyncAdapter(Context context, boolean autoInitialize) {
@@ -139,30 +105,96 @@ public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
         deletePhotoEnAttente();
 
         // Récupération du nombre d'utilisateur
-        Call<ReturnWS> callGetInfoServer = retrofitService.infoServer();
-        callGetInfoServer.enqueue(callbackGetInfoServer);
+        getInfoServer();
 
-        Log.d(TAG, "JPO ***** onPerformSync");
+        // Envoie d'une notification si on a envoyé quelque chose.
+        sendNotification(textToSend);
     }
 
+    private void sendNotification(String textToSend) {
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(mContext)
+                        .setSmallIcon(R.mipmap.ic_annonces)
+                        .setContentTitle(mContext.getString(R.string.app_name))
+                        .setContentText(textToSend);
 
+        // Sets an ID for the notification
+        int mNotificationId = 001;
 
-    private PhotoForWs getPhotoFromCursor(Cursor cursor){
-        PhotoForWs photo= new PhotoForWs();
+        // Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) mContext.getSystemService(NOTIFICATION_SERVICE);
+
+        // Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+
+    private Callback<ReturnWS> callbackPostAnnonce = new Callback<ReturnWS>() {
+        @Override
+        public void onResponse(Call<ReturnWS> call, Response<ReturnWS> response) {
+            if (response.isSuccessful()) {
+                ReturnWS rs = response.body();
+                if (rs.statusValid()) {
+                    // Si la mise à jour à bien eu lieu sur le serveur, on va mettre à jour l'annonce dans notre ContentProvider
+                    Integer idLocal = rs.getIdLocal();
+                    ContentValues contentValues = new ContentValues();
+                    contentValues.put(AnnonceContract.COL_STATUT_ANNONCE, StatutAnnonce.Valid.valeur());
+                    String where = AnnonceContract._ID + " = ?";
+                    String[] whereArgs = new String[]{String.valueOf(idLocal)};
+                    mContentResolver.update(AnnonceEntry.CONTENT_URI, contentValues, where, whereArgs);
+                } else {
+                    Toast.makeText(getContext(), rs.getMsg(), Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(Call<ReturnWS> call, Throwable t) {
+            t.printStackTrace();
+        }
+    };
+
+    private Callback<ReturnWS> callbackGetInfoServer = new Callback<ReturnWS>() {
+        @Override
+        public void onResponse(Call<ReturnWS> call, Response<ReturnWS> response) {
+            if (response.isSuccessful()) {
+                ReturnWS rs = response.body();
+                if (rs.statusValid()) {
+                    InfoServer infoServer = gson.fromJson(rs.getMsg(), InfoServer.class);
+                    ListeStats.setNbUsers(infoServer.getNbUtilisateur());
+                    ListeStats.setNbAnnonces(infoServer.getNbAnnonce());
+                    ListeCategories.setNbAnnonceFromHashMap(mContext, infoServer.getNbAnnonceByCategorie());
+                }
+            }
+        }
+
+        @Override
+        public void onFailure(Call<ReturnWS> call, Throwable t) {
+            Log.d(TAG, "callbackGetInfoServer failed");
+        }
+    };
+
+    private void getInfoServer() {
+        Call<ReturnWS> callGetInfoServer = retrofitService.infoServer();
+        callGetInfoServer.enqueue(callbackGetInfoServer);
+    }
+
+    private PhotoForWs getPhotoFromCursor(Cursor cursor) {
+        PhotoForWs photoForWs = new PhotoForWs();
         int indexColIdLocal = cursor.getColumnIndex(PhotoContract._ID);
         int indexColIdAnnonce = cursor.getColumnIndex(PhotoContract.COL_ID_ANNONCE);
         int indexColNomPhoto = cursor.getColumnIndex(PhotoContract.COL_NOM_PHOTO);
         int indexColIdPhoto = cursor.getColumnIndex(PhotoContract.COL_ID_PHOTO_SERVER);
 
-        photo.idLocal = cursor.getInt(indexColIdLocal);
-        photo.idAnnonce = cursor.getInt(indexColIdAnnonce);
-        photo.nomPhoto = cursor.getString(indexColNomPhoto);
-        photo.idPhoto = cursor.getInt(indexColIdPhoto);
+        photoForWs.idLocal = cursor.getInt(indexColIdLocal);
+        photoForWs.idAnnonce = cursor.getInt(indexColIdAnnonce);
+        photoForWs.nomPhoto = cursor.getString(indexColNomPhoto);
+        photoForWs.idPhoto = cursor.getInt(indexColIdPhoto);
 
-        return photo;
+        return photoForWs;
     }
 
-    private AnnonceForWs getAnnonceFromCursor(Cursor cursor){
+    private AnnonceForWs getAnnonceFromCursor(Cursor cursor) {
         AnnonceForWs annonceForWs = new AnnonceForWs();
         int indexColIdLocal = cursor.getColumnIndex(AnnonceContract._ID);
         int indexColIdAnnonce = cursor.getColumnIndex(AnnonceContract.COL_ID_ANNONCE_SERVER);
@@ -191,7 +223,7 @@ public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
 
         if (cursorPhotosToDelete != null) {
             while (cursorPhotosToDelete.moveToNext()) {
-                PhotoForWs photoForWs =  getPhotoFromCursor(cursorPhotosToDelete);
+                PhotoForWs photoForWs = getPhotoFromCursor(cursorPhotosToDelete);
                 retrofitService.deletePhoto(photoForWs.idAnnonce, photoForWs.idPhoto).enqueue(callbackPostAnnonce);
             }
             cursorPhotosToDelete.close();
@@ -206,7 +238,7 @@ public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
 
         if (cursorAnnoncesToDelete != null) {
             while (cursorAnnoncesToDelete.moveToNext()) {
-                AnnonceForWs annonceWs =  getAnnonceFromCursor(cursorAnnoncesToDelete);
+                AnnonceForWs annonceWs = getAnnonceFromCursor(cursorAnnoncesToDelete);
                 retrofitService.deleteAnnonce(annonceWs.idAnnonce).enqueue(callbackPostAnnonce);
             }
             cursorAnnoncesToDelete.close();
@@ -221,14 +253,14 @@ public class AnnoncesSyncAdapter extends AbstractThreadedSyncAdapter {
 
         if (cursorAnnoncesToSend != null) {
             while (cursorAnnoncesToSend.moveToNext()) {
-                AnnonceForWs annonceWs =  getAnnonceFromCursor(cursorAnnoncesToSend);
+                AnnonceForWs annonceWs = getAnnonceFromCursor(cursorAnnoncesToSend);
                 retrofitService.postAnnonce(annonceWs.idCategory,
                         annonceWs.idUtilisateur,
                         annonceWs.titreAnnonce,
                         annonceWs.descriptionAnnonce,
                         annonceWs.prixAnnonce,
                         annonceWs.idLocal).enqueue(callbackPostAnnonce);
-           }
+            }
             cursorAnnoncesToSend.close();
         }
     }
