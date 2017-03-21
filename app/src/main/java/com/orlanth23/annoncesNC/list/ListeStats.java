8293ff675cb844ad8dbc090042287dd0e3c99786
orlanth23 <orlanth23@hotmail.com>
2017-03-22 06:37:21 +1100

    Finir le ListeStats

--- app/src/main/java/com/orlanth23/annoncesNC/list/ListeStats.java
+++ app/src/main/java/com/orlanth23/annoncesNC/list/ListeStats.java
@@ -1,13 +1,19 @@
 package com.orlanth23.annoncesnc.list;
 
+import android.content.Context;
+
+import com.orlanth23.annoncesnc.provider.ProviderContract;
+
 public class ListeStats {
 
     private static ListeStats INSTANCE = null;
     private static Integer nbAnnonces;
     private static Integer nbUsers;
 
-    public static ListeStats getInstance() {
+    public static ListeStats getInstance(Context context) {
         if (INSTANCE == null) {
+            // ToDo finir l'appel du Query
+            context.getContentResolver().query(ProviderContract.InfosServerEntry.CONTENT_URI, projection, selection, selectionArgs, sortBy);
             INSTANCE = new ListeStats();
         }
         return INSTANCE;
